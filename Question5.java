92388cd3a428bedd4fc7cfa1adf7b532e04ed7c1
Ryanyzh <92764860+Ryanyzh@users.noreply.github.com>
2022-04-21 21:10:04 +0800

    edit question 5

--- Question5.java
+++ Question5.java
@@ -1,69 +1,21 @@
-import java.util.Scanner;
-import java.util.List;
-import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.*;
 
-public class Question5
-{
-  /*
-  static int mode(List<Integer> a)
-  {
-    int maxValue = 0, maxCount = 0, x, y;
-    int n = a.size();
-    for(x = 0; x < n; x++)
-    {
-      int count = 0;
-      for(y = 0; y < n; y++)
-      {
-        if (a.get(y) == a.get(x))
-        {
-          count++;
-        }
-      }
-      if (count > maxCount)
-      {
-        maxCount = count;
-        maxValue = a.get(x);
-      }
-    }
-    return maxValue;
-  }
-  */
-  public static void main(String[] args)
-  {
-    /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
-     *    e.g.
-     *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
-     * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
-     */
-     
-    Scanner in = new Scanner(System.in);
-    int numInput = in.nextInt();
+public class Question5 {
+  public static void main(String[] args) {
+  	Scanner objOne = new Scanner(System.in);  // Create a Scanner object
+    System.out.println("");
+    int numInput = objOne.nextInt();  
     ArrayList<Integer> lista = new ArrayList<Integer>();
-
-    for(int i = 0; i < numInput; i++)
+    
+    for (int i = 0; i < numInput; i++)
     {
-      Scanner input = new Scanner(System.in);
-      int number = input.nextInt();
-      lista.add(number);
+        Scanner objTwo = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("");
+        int number = objTwo.nextInt();  
+        lista.add(number);
     }
-
+    
+    
     int maxValue = 0, maxCount = 0, x, y;
     int n = lista.size();
     for(x = 0; x < n; x++)
@@ -85,6 +37,6 @@ public class Question5
     }
 
     System.out.println(maxValue);
-    
   }
+    
 }
